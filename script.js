1f5b2b3822a75d948fe85fc032bb5b8202aa0085
YasirBajwa <yasirbajwa369@gmail.com>
2020-06-27 22:41:51 +0500

    update calculator

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ function onlyNumberKey(evt) {
      return false;   
     return true; 
 }
+
+function defaultValue(){
+    document.getElementById('result').defaultValue = 0;
+}
+function resetValue (){
+    document.getElementById('result').value = '';
+
+}
+function resetDefaultValue(){
+    document.getElementById('result').defaultValue = '';
+
+}
+function getValue(num){
+    resetDefaultValue();
+    var result = document.getElementById('result');
+    
+    result.value += num
+     
+
+}
+function getResult(){
+    var result = document.getElementById('result');
+
+   result.value = eval(result.value)
+   
+
+}
+function clearInput(){
+    var result = document.getElementById('result');
+    result.value = ''
+}
